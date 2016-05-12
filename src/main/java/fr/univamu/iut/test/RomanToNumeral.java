ec2b34aa30668a10df2a8b9bd1a9fb4214b8353b
cybermoi <arthur.cauchy@free.fr>
2016-05-12 11:29:38 +0200

    voila

--- src/main/java/fr/univamu/iut/test/RomanToNumeral.java
+++ src/main/java/fr/univamu/iut/test/RomanToNumeral.java
@@ -1,6 +1,4 @@
-package fr.univamu.iut.test;//Franchement ça marche super bien git!!
-
-//grosse merde
+package fr.univamu.iut.test;
 
 public class RomanToNumeral {
 	public static int processDecimal(int decimal, int lastNumber, int lastDecimal) {
